cdaca22cc4b2887dc1cf7b2a0bc26db12fb3a4d4
Brian Kipkemboi <briankipkemboi47@gmail.com>
2022-10-14 04:04:06 -0700

    This is size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - this is the entry point
+ *
+ * Return: Always should be 0 to show success
+ */
+int main(void){
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
