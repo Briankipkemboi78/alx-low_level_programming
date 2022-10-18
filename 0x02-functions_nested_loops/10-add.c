5c97f2dec0e91fa6f8d897c6fa4d672bcc2c08dd
Brian Kipkemboi <briankipkemboi47@gmail.com>
2022-10-18 09:55:26 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *add - add two digits together
+ *
+ *Description: x+y
+ *
+ *@a:input
+ *
+ *@b:input
+ *
+ *Return:sum of input int
+ *
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
