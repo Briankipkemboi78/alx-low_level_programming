046028e17e70b72c1e74e444c5bcbfa3fff6e758
Brian Kipkemboi <briankipkemboi47@gmail.com>
2022-10-18 04:35:29 -0700

    one and zeros

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_islower - checks for lowercase characters
+ *
+ *Description: checks for lowercase
+ *
+ *Return: 1 - lowercase 0 - otherwise
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
