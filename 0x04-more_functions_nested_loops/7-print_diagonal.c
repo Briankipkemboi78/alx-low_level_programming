726e77c59ea9417b39a70b6f2801e55faf3fd8e5
Brian Kipkemboi <briankipkemboi47@gmail.com>
2022-10-21 05:56:08 -0700

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagonal - print diagonal line dependent on the integer n
+* @n: The number of lines using '\' character to use
+* Return : void
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int space;
+
+	for (i = 0; i < n; i++)
+	{
+		for (space = 0; space < i; space++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
