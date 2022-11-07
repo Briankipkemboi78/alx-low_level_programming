77250f999ea7ebbfa3dac99bf13b53aa44a4588c
Brian Kipkemboi <briankipkemboi47@gmail.com>
2022-11-07 20:50:07 +0300

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - multiplies two numbers
+*
+*@argc:arguments count
+*@argv:arguments vector
+*
+*Return: 0
+*
+*/
+int main(int argc, char *argv[])
+{
+	int product = 0;
+	int x, y;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		product = x * y;
+		printf("%d\n", product);
+		return (0);
+	}
+	else
+	{
+		printf("Err\n")[D[D[D[D[D[Cor\n");
+		return (1);
+	}
+}
