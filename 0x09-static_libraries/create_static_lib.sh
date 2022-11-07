e0c13a96725b862c301e0c864a8b9e7a1a1d960e
Brian Kipkemboi <114360135+Briankipkemboi78@users.noreply.github.com>
2022-11-07 23:07:43 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-gcc -c [1;2j*.c
-ar rc liball.a *.o
-rm *.o
-||||||| 9b46de3
 gcc -Wall -Werror -Wextra -std=gnu89 -c *.c
 ar -rc liball.a *.o
 ranlib liball.a
-
-=======
-gcc -c *.c
-ar rc liball.a *.o
-rm *.o
->>>>>>> 993fcd2a71a31890d985055af2cc012a9ae34e98
