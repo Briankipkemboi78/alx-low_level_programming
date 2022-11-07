efdc0af0acfa905fb12b10cb53b1029186097d5d
Brian Kipkemboi <briankipkemboi47@gmail.com>
2022-11-07 21:13:17 +0300

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*
+*main - add two positive numbers
+*
+*@argc:argument count
+*@argv:argument vector
+*
+*Return: 0
+*
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+[A		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+}
