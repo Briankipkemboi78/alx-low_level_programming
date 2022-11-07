63614050773028fb4007737155499dc0ba20ebaa
Brian Kipkemboi <114360135+Briankipkemboi78@users.noreply.github.com>
2022-11-07 21:02:30 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -25,8 +25,7 @@ int main(int argc, char *argv[])
 	{
 		j = 1;
 
-[A[A[B[3~[3~[C[C[C[C[C[C[C[C[C[C
-[A[		for (i = 1; i < 3; i++)
+		for (i = 1; i < 3; i++)
 		j *= atoi(argv[i]);
 
 		printf("%d\n", j);
